d0d11a02186113eba56407091cf9ee54f62f9744
Caroline Manson <carolinemanson15@gmail.com>
2020-12-20 17:41:55 -0500

    built html routes

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const path = require("path");
 // starts up express app
 const app = express();
 
-// i don't know what this does
 app.use(logger("dev"));
 
-app.use(express.urlencoded({ extended: true }));
+// middleware
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 app.use(express.static("public"));
@@ -23,40 +25,51 @@ const collections = ["workouts"];
 const db = mongojs(databaseUrl, collections);
 
 db.on("error", error => {
-  console.log("Database Error:", error);
+    console.log("Database Error:", error);
 });
 
-// get default homepage
+// html routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "./public/index.html"));
-  });
-
-// view all exercises on stats page
-// or should the found argument be data?
-app.get("/stats", (req, res) => {
-  db.workouts.find({}, (err, found) => {
-    if (err) {
-      console.log(err);
-    } else {
-        // this retrieves everything out of our database
-      res.json(found);
-    }
-  });
 });
 
-// submit new exercise to homepage
-app.post("/submit", (req, res) => {
-    console.log(req.body);
-  
-    db.workouts.insert(req.body, (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    });
-  });
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+
+
+// // view all exercises on stats page
+// // or should the found argument be data?
+// app.get("/stats", (req, res) => {
+//   db.workouts.find({}, (err, found) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//         // this retrieves everything out of our database
+//       res.json(found);
+//     }
+//   });
+// });
+
+// does this need to be /submit or /click
+// // submit new exercise to homepage
+// app.post("/submit", (req, res) => {
+//     console.log(req.body);
+
+//     db.workout.insert(req.body, (error, data) => {
+//       if (error) {
+//         res.send(error);
+//       } else {
+//         res.send(data);
+//       }
+//     });
+//   });
 
 app.listen(3000, () => {
-  console.log("App running on port 3000!");
+    console.log("App running on port 3000!");
 });
